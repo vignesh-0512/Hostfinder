5d1b2e34cf699ac194c6f680847f8cfa7572d34b
T Vignesh <115525353+vignesh-0512@users.noreply.github.com>
2024-03-10 12:15:17 +0530

    Update hostfinder.py

--- hostfinder.py
+++ hostfinder.py
@@ -2,7 +2,7 @@ import socket
 try:
     host = input("Enter website name:")
     ip=socket.gethostbyname(host)
-    print("Ip address for ",host,"is",ip)
+    print("Ip address for",host,ip)
 
 
 except:
